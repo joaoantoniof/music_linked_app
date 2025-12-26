c957306adbd8734b7e6b2732e99972e2c4955eba
joaoantonio <joaoantonio.ferreira@prozis.com>
2025-12-26 16:52:18 +0000

    Corrections

--- my_database_app/app/index.tsx
+++ my_database_app/app/index.tsx
@@ -1,15 +1,41 @@
 import { Text, View } from "react-native";
+import React, {useState} from 'react';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { auth } from '../FirebaseConfig'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 
 export default function Index() {
+  const [email, setEmail ] = useState('');
+  const [password, setPassword ] = useState('');
+
+  const signIn = async() => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password)
+    } catch (error: any) {
+      console.log(error)
+      alert('Sign in failed: ' + error.message);
+    }
+  }
+
+  const signUp = async() => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password)
+    } catch (error: any) {
+      console.log(error)
+      alert('Sign in failed: ' + error.message);
+    }
+  }
+
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>My APP</Text>
-    </View>
+      <Text>LOGIN PAGE</Text>
+    </SafeAreaView>
   );
 }
